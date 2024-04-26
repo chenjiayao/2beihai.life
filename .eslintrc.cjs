0d5a514b038e0394097410752162f47514816c23
chenjiayao <chenjiayao@gk.cn>
2024-04-26 12:29:02 +0800

    lint fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,7 @@ const config = {
       rules: {
         '@typescript-eslint/no-misused-promises': 'off',
         '@typescript-eslint/no-unsafe-call': 'off',
+        'simple-import-sort/imports':'off',
         '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-unsafe-assignment': 'off',
       },
