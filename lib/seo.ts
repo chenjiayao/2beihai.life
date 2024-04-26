5467d8f7bf52067078d51af23bafdcf41149b2b3
chenjiayao <chenjiayao@gk.cn>
2024-04-26 13:41:21 +0800

    lint fix

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,10 +1,10 @@
 export const seo = {
-  title: 'Cali Castle | 开发者、设计师、细节控、创始人',
+  title: 'JayChen | 普通人、开发者',
   description:
-    '我叫 Cali，一名开发者，设计师，细节控，同时也是佐玩创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。',
+    '我叫 JayChen，一个普通人哈哈哈哈哈',
   url: new URL(
     process.env.NODE_ENV === 'production'
-      ? 'https://cali.so'
+      ? 'https://2beihai.life'
       : 'http://localhost:3000'
   ),
 } as const
