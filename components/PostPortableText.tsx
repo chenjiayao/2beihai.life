91c3e24864e7ec33f0453b6479c6fa598306aba0
chenjiayao <chenjiayao@gk.cn>
2024-09-26 14:09:44 +0800

    initial

--- components/PostPortableText.tsx
+++ components/PostPortableText.tsx
@@ -1,17 +1,17 @@
 'use client'
 
 import React from 'react'
-import ReactMarkdown from 'react-markdown'
+import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
-import rehypeRaw from 'rehype-raw'
 
 export function PostPortableText(props: {
   value: string // 现在 value 应该是一个字符串，而不是 any
 }) {
   return (
-    <ReactMarkdown   remarkPlugins={[remarkGfm]}
-    rehypePlugins={[rehypeRaw]}>
-      {props.value}
-    </ReactMarkdown>
+    <Markdown
+    remarkPlugins={[remarkGfm]}
+  >
+    {props.value}
+  </Markdown>
   )
 }
